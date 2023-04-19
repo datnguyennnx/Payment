e712e8d49da2622c3589554151d17f472c049265
Nguyen Dat <official.nguyendat@gmail.com>
2023-04-19 08:39:08 +0700

    Implementation codebase

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
