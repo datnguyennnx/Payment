9716a31397b374919542b9970b62d8991e893ddd
Nguyen Dat <official.nguyendat@gmail.com>
2023-04-23 21:05:25 +0700

    UX/UI: Fix responsive textarea and scale high text fixed row in container

--- src/components/Form/DonateForm/DonateForm.tsx
+++ src/components/Form/DonateForm/DonateForm.tsx
@@ -1,31 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { TbCoffee } from 'react-icons/tb'
+import useAutosizeTextArea from "./useAutosizeTextArea";
+
 
 export function DonateForm (){
     const [amount, setAmount] = useState<any>(0)
 
     const radioHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        return setAmount(event.target.value);
-    };
+        return setAmount(event.target.value)
+    }
 
     const formHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        return setAmount(event.target.value);
+        return setAmount(event.target.value)
+    }
+
+    const [value, setValue] = useState("");
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+    useAutosizeTextArea(textAreaRef.current, value);
+
+    const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const val = evt.target?.value;
+
+        setValue(val);
     };
 
-    const [message, setMessage] = useState('');
-    const handleMessageChange = event  => {
-        setMessage(event.target.value);
-        console.log(event.target.value);
-      };
-
     return (
-        <div className="flex w-[30%] p-4">
-            <div className="flex w-full h-[40rem] bg-[#f3f1e5] border-[1px] rounded-xl border-gray-400 ">
+        <div className="flex min-w-[28rem] max-w-[28rem] p-4">
+            <div className="flex w-full h-auto bg-[#f3f1e5] border-[1px] rounded-xl border-gray-400 ">
                 <div className="w-full">
 
                     <p className="flex m-8 text-2xl font-semibold w-auto"> Buy Nguyễn Đạt a coffee</p>
                 
-                    <div className="flex w-auto m-2 p-2 h-20 border-[1px] rounded-xl border-black bg-[#ffe5c8]">
+                    <div className="flex w-auto m-6 p-2 h-20 border-[1px] rounded-xl border-black bg-[#ffe5c8]">
                         <div className="flex flex-row self-center">
 
                             <div className="mr-2 ml-2 self-center ">
@@ -83,18 +90,20 @@ export function DonateForm (){
                         </div>
                     </div>
 
-                    <p className="flex m-4 text-2xl font-semibold w-auto"> Question is</p>
+                    <p className="flex mt-2 mr-8 ml-8 text-2xl font-semibold w-auto"> Question</p>
 
-                    <div className="flex w-auto p-2 h-auto "> 
-                        <textarea id="message" name="message" value={message} 
-                        onChange={handleMessageChange} cols={30} rows={8} 
-                        className="w-full bg-[#f3f1e5] text-2xl p-4 border-2 rounded-xl border-black" 
+                    <div className="flex w-auto p-6 h-auto "> 
+                        <textarea id="message" name="message" value={value} cols={30} rows={2}
+                        onChange={handleChange}
+                        ref={textAreaRef}
+                        className="flex w-full bg-[#f3f1e5] text-2xl p-4 border-2 
+                        max-h-[25rem] rounded-xl border-black" 
                         placeholder="What ever you want to ask"></textarea>
                     </div>
 
-                    <div className="flex w-auto m-2 h-10 border-[1px] 
+                    <div className="flex w-auto mt-2 mr-8 ml-8 h-10 border-[1px] 
                             rounded-xl bg-[#ff813f] justify-center" >
-                        <button className="text-2xl" disabled={!message}>{"Support " + amount*5 + " $"}</button>
+                        <button className="text-2xl" disabled={!value}>{"Support " + amount*5 + " $"}</button>
                     </div>
                 </div>
             </div>
